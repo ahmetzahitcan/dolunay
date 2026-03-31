73723f22663bb624afe79810911502c6053bbb3e
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-03-31 10:32:49 +0300

    added "skip warp" feature to warp scheduler

--- src/warp_scheduler.sv
+++ src/warp_scheduler.sv
@@ -3,14 +3,23 @@ module warp_scheduler
 (
     input logic clk,
     input logic rst_n,
-    output logic [LOG_NUM_WARPS-1:0] warp_id_p0_o,
-    output logic [LOG_NUM_WARPS-1:0] warp_id_p1_o
+    input logic skip_i,
+    input logic [LOG_NUM_WARPS-1:0] skip_warp_id_i,
+    output logic [LOG_NUM_WARPS-1:0] warp_id_o
 );
 
 logic [LOG_NUM_WARPS-1:0] current_warp_r;
-logic [LOG_NUM_WARPS-1:0] next_warp_w;
+logic [LOG_NUM_WARPS-1:0] current_warp_p1_w;
+logic [LOG_NUM_WARPS-1:0] current_warp_p2_w;
 
-assign next_warp_w = current_warp_r + 1'b1;
+assign current_warp_p1_w = current_warp_r + 1'b1;
+assign current_warp_p2_w = current_warp_r + 2'b10;
+
+logic skip_w;
+assign skip_w = skip_i && (skip_warp_id_i == current_warp_p1_w);
+
+logic [LOG_NUM_WARPS-1:0] next_warp_w;
+assign next_warp_w = skip_w ? current_warp_p2_w : current_warp_p1_w;
 
 always_ff @(posedge clk) begin
     if (!rst_n) begin
@@ -18,9 +27,8 @@ always_ff @(posedge clk) begin
     end else begin
         current_warp_r <= next_warp_w;
     end
-    
-    warp_id_p0_o <= current_warp_r;
-    warp_id_p1_o <= next_warp_w;
 end
+
+assign warp_id_o = current_warp_r;
 
 endmodule
