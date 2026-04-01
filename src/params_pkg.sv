1b47a8ffcbd815b438c76fae10dad8ab9f901ca0
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-04-01 15:20:10 +0300

    changed parameter to localparam in params_pkg

--- src/params_pkg.sv
+++ src/params_pkg.sv
@@ -1,21 +1,21 @@
 package params_pkg;
-// TODO: use localparam instead of parameter
-    parameter int XLEN = 32;
 
-    parameter int ADDR_WIDTH = 32;
-    parameter int ADDR_ALIGN = 4;
-    parameter int LOG_ADDR_ALIGN = $clog2(ADDR_ALIGN);
+    localparam int XLEN = 32;
 
-    parameter int PC_ALIGN = 4;
-    parameter int LOG_PC_ALIGN = $clog2(PC_ALIGN);
+    localparam int ADDR_WIDTH = 32;
+    localparam int ADDR_ALIGN = 4;
+    localparam int LOG_ADDR_ALIGN = $clog2(ADDR_ALIGN);
 
-    parameter int NUM_WARPS = 4;
-    parameter int NUM_THREADS = 8;
-    parameter int NUM_REGISTERS = 16; // RV32E
-    parameter int NUM_BARRIERS = 2; // Max 8
-    parameter int LOG_NUM_WARPS = $clog2(NUM_WARPS);
-    parameter int LOG_NUM_THREADS = $clog2(NUM_THREADS);
-    parameter int LOG_NUM_REGISTERS = $clog2(NUM_REGISTERS);
-    parameter int LOG_NUM_BARRIERS = $clog2(NUM_BARRIERS);
+    localparam int PC_ALIGN = 4;
+    localparam int LOG_PC_ALIGN = $clog2(PC_ALIGN);
+
+    localparam int NUM_WARPS = 4;
+    localparam int NUM_THREADS = 8;
+    localparam int NUM_REGISTERS = 16; // RV32E
+    localparam int NUM_BARRIERS = 2; // Max 8
+    localparam int LOG_NUM_WARPS = $clog2(NUM_WARPS);
+    localparam int LOG_NUM_THREADS = $clog2(NUM_THREADS);
+    localparam int LOG_NUM_REGISTERS = $clog2(NUM_REGISTERS);
+    localparam int LOG_NUM_BARRIERS = $clog2(NUM_BARRIERS);
     
 endpackage
