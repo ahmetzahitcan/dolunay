7b999a31f61e37c63ecf991235b27945cf1c8e65
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-04-06 14:50:04 +0300

    fixed issues in mem formatters, and made them multiple data

--- src/mem_read_formatter.sv
+++ src/mem_read_formatter.sv
@@ -1,68 +1,86 @@
 module mem_read_formatter 
     import params_pkg::*;
     import control_unit_pkg::*;
-(
+#(
+    parameter int DATA_LEN = N_THREADS
+)(
     input wire  mem_opsize_e opsize_i,
     input wire  mem_extendmode_e extendmode_i,
-    input wire  logic [XLEN-1:0] m_data_i,
-    input wire  logic [Z_ADDR-1:0] alignment_i,
-    output logic [XLEN-1:0] p_data_o
-)
+    input wire  logic [DATA_LEN-1:0][XLEN-1:0] m_data_i,
+    input wire  logic [DATA_LEN-1:0][Z_ADDR-1:0] alignment_i,
+    output logic [DATA_LEN-1:0][XLEN-1:0] p_data_o
+) ;
 
     `ifndef SYNTHESIS
-        initial assert (ADDR_ALIGN == 4) else $error("Only ADDR_ALIGN = 4 is supported");
+        initial begin
+            assert (ADDR_ALIGN == 4) else $error("Only ADDR_ALIGN = 4 is supported");
+        end
     `endif
 
-    logic [7:0] byte_w;
+    logic [DATA_LEN-1:0][7:0] byte_w;
     always_comb begin
-        case (alignment_i) begin
-            2'b00: byte_w = m_data_i[7:0];
-            2'b01: byte_w = m_data_i[15:8];
-            2'b10: byte_w = m_data_i[23:16];
-            2'b11: byte_w = m_data_i[31:24];
-            default: byte_w = 'x;
-        endcase
-    end
-
-    logic [15:0] half_w;
-    always_comb begin
-        case (alignment_i) begin
-            2'b00: half_w = m_data_i[15:0];
-            2'b10: half_w = m_data_i[31:16];
-            default: half_w = 'x;
-        endcase
-    end
-
-    logic [31:0] word_w;
-    always_comb begin
-        case (alignment_i) begin
-            2'b00: word_w = m_data_i[31:0];
-            default: word_w = 'x;
-        endcase
-    end
-
-    logic sign_w;
-    always_comb begin
-        case (opsize_i) begin
-            MEM_OPSIZE_BYTE: sign_w = byte_w[7];
-            MEM_OPSIZE_HALF: sign_w = half_w[15];
-            MEM_OPSIZE_WORD: sign_w = word_w[31];
-            default: sign_w = 'x;
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (alignment_i[i])
+                2'b00: byte_w[i] = m_data_i[i][7:0];
+                2'b01: byte_w[i] = m_data_i[i][15:8];
+                2'b10: byte_w[i] = m_data_i[i][23:16];
+                2'b11: byte_w[i] = m_data_i[i][31:24];
+                default: byte_w[i] = 'x;
+            endcase
         end
     end
 
-    logic extend_w;
-    assign extend_w = extendmode_i == MEM_EXTENDMODE_SIGN ? sign_w : 1'b0;
+    logic [DATA_LEN-1:0][15:0] half_w;
+    always_comb begin
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (alignment_i[i])
+                2'b00: half_w[i] = m_data_i[i][15:0];
+                2'b10: half_w[i] = m_data_i[i][31:16];
+                default: half_w[i] = 'x;
+            endcase
+        end
+    end
 
-    logic [XLEN-1:0] reg_data_w;
+    logic [DATA_LEN-1:0][31:0] word_w;
+    always_comb begin
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (alignment_i[i])
+                2'b00: word_w[i] = m_data_i[i][31:0];
+                default: word_w[i] = 'x;
+            endcase
+        end
+    end
+
+    logic [DATA_LEN-1:0] sign_w;
+    always_comb begin
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (opsize_i)
+                MEM_OPSIZE_BYTE: sign_w[i] = byte_w[i][7];
+                MEM_OPSIZE_HALF: sign_w[i] = half_w[i][15];
+                MEM_OPSIZE_WORD: sign_w[i] = word_w[i][31];
+                default: sign_w[i] = 'x;
+            endcase
+        end
+    end
+
+    logic [DATA_LEN-1:0] extend_w;
+    always_comb begin
+        for (int i = 0; i < DATA_LEN; i++) begin
+            extend_w[i] = extendmode_i == MEM_EXTENDMODE_SIGN ? sign_w[i] : 1'b0;
+        end
+    end
+
+    logic [DATA_LEN-1:0][XLEN-1:0] reg_data_w;
 
     always_comb begin
-        case (opsize_i) inside
-            MEM_OPSIZE_BYTE: reg_data_w = {{XLEN-8{extend_w}}, byte_w};
-            MEM_OPSIZE_HALF: reg_data_w = {{XLEN-16{extend_w}}, half_w};
-            MEM_OPSIZE_WORD: reg_data_w = {{XLEN-32{extend_w}}, word_w};
-            default: reg_data_w = 'x;
-        endcase
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (opsize_i)
+                MEM_OPSIZE_BYTE: reg_data_w[i] = {{XLEN-8{extend_w[i]}}, byte_w[i]};
+                MEM_OPSIZE_HALF: reg_data_w[i] = {{XLEN-16{extend_w[i]}}, half_w[i]};
+                MEM_OPSIZE_WORD: reg_data_w[i] = {{XLEN-32{extend_w[i]}}, word_w[i]};
+                default: reg_data_w[i] = 'x;
+            endcase
+        end
     end
 
     assign p_data_o = reg_data_w;
--- src/mem_write_formatter.sv
+++ src/mem_write_formatter.sv
@@ -1,51 +1,59 @@
 module mem_write_formatter
     import params_pkg::*;
     import control_unit_pkg::*;
-(
-    input wire logic p_data_i;
-    input wire mem_opsize_e opsize_i;
-    input wire logic [Z_ADDR-1:0] alignment_i;
-    output logic [XLEN-1:0] m_data_o;
-    output logic [ADDR_ALIGN-1:0] m_wen_o;
+#(
+    parameter int DATA_LEN = N_THREADS
+)(
+    input wire logic [DATA_LEN-1:0][XLEN-1:0] p_data_i,
+    input wire mem_opsize_e opsize_i,
+    input wire logic [DATA_LEN-1:0][Z_ADDR-1:0] alignment_i,
+    output logic [DATA_LEN-1:0][XLEN-1:0] m_data_o,
+    output logic [DATA_LEN-1:0][ADDR_ALIGN-1:0] m_wen_o
 );
 
     `ifndef SYNTHESIS
-        initial assert (ADDR_ALIGN == 4) else $error("Only ADDR_ALIGN = 4 is supported");
+        initial begin
+            assert (ADDR_ALIGN == 4) else $error("Only ADDR_ALIGN = 4 is supported");
+        end
     `endif
 
-    logic [XLEN-1:0] m_data_w;
+    logic [DATA_LEN-1:0][XLEN-1:0] m_data_w;
 
     always_comb begin
-        case (opsize_i) begin
-            MEM_OPSIZE_BYTE: m_data_w = {4{p_data_i[7:0]}};
-            MEM_OPSIZE_HALF: m_data_w = {2{p_data_i[15:0]}};
-            MEM_OPSIZE_WORD: m_data_w = p_data_i;
-            default: m_data_w = 'x;
-        endcase
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (opsize_i)
+                MEM_OPSIZE_BYTE: m_data_w[i] = {4{p_data_i[i][7:0]}};
+                MEM_OPSIZE_HALF: m_data_w[i] = {2{p_data_i[i][15:0]}};
+                MEM_OPSIZE_WORD: m_data_w[i] = p_data_i[i];
+                default: m_data_w[i] = 'x;
+            endcase
+        end
     end
 
-    logic [ADDR_ALIGN-1:0] m_wen_w;
+    logic [DATA_LEN-1:0][ADDR_ALIGN-1:0] m_wen_w;
 
     always_comb begin
-        case (opsize_i) begin
-            MEM_OPSIZE_BYTE: case (alignment_i) begin
-                2'b00: m_wen_w = 4'b0001;
-                2'b01: m_wen_w = 4'b0010;
-                2'b10: m_wen_w = 4'b0100;
-                2'b11: m_wen_w = 4'b1000;
-                default: m_wen_w = 'x;
+        for (int i = 0; i < DATA_LEN; i++) begin
+            case (opsize_i)
+                MEM_OPSIZE_BYTE: case (alignment_i[i])
+                    2'b00: m_wen_w[i] = 4'b0001;
+                    2'b01: m_wen_w[i] = 4'b0010;
+                    2'b10: m_wen_w[i] = 4'b0100;
+                    2'b11: m_wen_w[i] = 4'b1000;
+                    default: m_wen_w[i] = 'x;
+                endcase
+                MEM_OPSIZE_HALF: case (alignment_i[i])
+                    2'b00: m_wen_w[i] = 4'b0011;
+                    2'b10: m_wen_w[i] = 4'b1100;
+                    default: m_wen_w[i] = 'x;
+                endcase
+                MEM_OPSIZE_WORD: case (alignment_i[i])
+                    2'b00: m_wen_w[i] = 4'b1111;
+                    default: m_wen_w[i] = 'x;
+                endcase
+                default: m_wen_w[i] = 'x;
             endcase
-            MEM_OPSIZE_HALF: case (alignment_i) begin
-                2'b00: m_wen_w = 4'b0011;
-                2'b10: m_wen_w = 4'b1100;
-                default: m_wen_w = 'x;
-            endcase
-            MEM_OPSIZE_WORD: case (alignment_i) begin
-                2'b00: m_wen_w = 4'b1111;
-                default: m_wen_w = 'x;
-            endcase
-            default: m_wen_w = 'x;
-        endcase
+        end
     end
 
     assign m_data_o = m_data_w;
