46a0a18c67769653255108265342a3199d92f84d
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-04-07 16:49:09 +0300

    working on shared memory

--- kernels/shared_mem_test.s
+++ kernels/shared_mem_test.s
@@ -2,11 +2,14 @@
 
 start: 
     csrr x1, mhartid
-    bnez x1, others
+    srli x2, x1, 16
+    bnez x2, halt
+    andi x2, x1, 0xff
+    bnez x2, others
 thread0:
     li x2, 0x0badbabe
     sw x2, 0(x0)
-    j .
+    .word 0b00100000000000000000000000001011 # yield
 others:
     li x2, 100
 waste_time:
@@ -14,4 +17,5 @@ waste_time:
     bnez x2, waste_time
 read_value:
     lw x3, 0(x0)
-    j .
+halt:
+    j halt
