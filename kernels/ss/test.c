4c222ff25f5cb9520dd8488e9d53616dbda271db
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-04-30 12:00:03 +0300

    adding tls_heap support and verifying the assembly output (its a bit weird at the end)

--- kernels/ss/test.c
+++ kernels/ss/test.c
@@ -1,7 +1,9 @@
 #include "simt.h"
 #include "hpm.h"
+#include "tls_heap.h"
 
 #define ARRAY_SIZE 4096
+#define TLS_ARRAY_SIZE 256
 
 simt_barr_t barr;
 
@@ -10,6 +12,20 @@ int main(void) {
     uint64_t *arr = (uint64_t*)0x40000000;
     for(int i = simt_thread_id() + simt_warp_id() * WARP_SIZE; i < ARRAY_SIZE; i += WARP_COUNT * WARP_SIZE) {
         arr[i] = hpm_cycle();
+    }
+    simt_bsync(&barr);
+
+    simt_binit(&barr);
+    uint64_t *tls_arr = (uint64_t*)tls_heap_alloc(TLS_ARRAY_SIZE * sizeof(uint64_t));
+    for(int i = 0; i < TLS_ARRAY_SIZE; i++) {
+        tls_arr[i] = hpm_cycle();
+    }
+    simt_bsync(&barr);
+
+    simt_binit(&barr);
+    if(simt_thread_is_follower()) {
+        *((uint64_t*)0x40004000) = hpm_wtinstret();
+        *((uint64_t*)0x40004008) = hpm_wuinstret();
     }
     simt_bsync(&barr);
     return 0;
---
+++ kernels/ss/tls_heap.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "stdint.h"
+#include "stddef.h"
+
+// TODO: Separate implementation and declaration.
+
+// FIXME: Get these from the linker script.
+#define TLS_HEAP_BASE (0x80000000)
+#define TLS_HEAP_SIZE (64 * 1024)
+
+__thread char *tls_heap_ptr = (char *)TLS_HEAP_BASE;
+__thread size_t tls_heap_size = TLS_HEAP_SIZE;
+
+static void *tls_heap_alloc(size_t size) {
+    if(size > tls_heap_size) {
+        return NULL;
+    }
+    void *ret = (void *)tls_heap_ptr;
+    tls_heap_ptr += size;
+    tls_heap_size -= size;
+    return ret;
+}
+
+static void tls_heap_free(void *ptr) {
+    // TODO: Create a way to free memory.
+    (void)ptr;
+}
