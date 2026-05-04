9b361c4ab401981bd761ac8dc76f5d270caec2df
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-05-04 10:15:32 +0300

    create crt0.s

---
+++ kernels/ss/crt0.s
@@ -0,0 +1,11 @@
+.section .text.init
+.global _start
+
+_start:
+    // TODO: Setup the stack
+    // TODO: Copy globals into WRAM
+    // TODO: Clear BSS
+    // TODO: Copy thread-local globals into TLS
+    // TODO: Clear TBSS
+    // TODO: Call main
+    // TODO: Halt
