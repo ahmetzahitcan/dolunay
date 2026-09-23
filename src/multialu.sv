ac2a57e171045b8f59846c47ea698456467b6243
Ahmet Zahit Can <ahmetzahit.can@yildiz.edu.tr>
2026-09-23 17:35:29 +0300

    remove the FIFO in multialu.sv, that was also a mistake

--- src/multialu.sv
+++ src/multialu.sv
@@ -5,9 +5,7 @@
 module multialu
     import params_pkg::*;
     import control_unit_pkg::*;
-#(
-    parameter FIFO_DEPTH = N_WARPS
-) (
+(
     input wire logic clk,
     input wire logic rst_n,
 
@@ -31,78 +29,36 @@ module multialu
     output op_tag_s op_tag_o,
     output logic [N_THREADS-1:0][RLEN-1:0] result_o
 );
-    localparam W_FIFO = $clog2(FIFO_DEPTH);
+    logic out_valid_r, out_valid_next_w, out_hold_w;
+    logic in_ready_w, in_fire_w;
 
-    logic [N_THREADS-1:0][RLEN-1:0] alu_result_w;
-    logic [FIFO_DEPTH-1:0][N_THREADS-1:0][RLEN-1:0] fifo_results_r;
-    op_tag_s [FIFO_DEPTH-1:0] fifo_tags_r;
-
-    logic [W_FIFO-1:0] fifo_head_r, fifo_head_p1_w;
-    logic [W_FIFO-1:0] fifo_exctail_r, fifo_exctail_p1_w; // Exclusive tail, that is, the tail pointer plus 1.
-    logic fifo_almost_empty_w, fifo_almost_full_w;
-    logic fifo_empty_r, fifo_full_r;
-
-    always_comb begin
-        fifo_head_p1_w = fifo_head_r + 1;
-        fifo_exctail_p1_w = fifo_exctail_r + 1;
-        fifo_almost_empty_w = (fifo_head_p1_w == fifo_exctail_r);
-        fifo_almost_full_w = (fifo_head_r == fifo_exctail_p1_w);
-    end
-
-    // Input handshake
-
-    logic in_handshake_w;
-    assign in_ready_o = ~fifo_full_r;
-    assign in_handshake_w = in_valid_i & in_ready_o;
+    assign in_fire_w = in_valid_i & in_ready_w;
+    assign out_hold_w = out_valid_r & !out_ready_i;
+    assign out_valid_next_w = in_fire_w || out_hold_w;
+    assign in_ready_w = !out_hold_w;
 
     always_ff @(posedge clk) begin
         if (!rst_n) begin
-            fifo_exctail_r <= 0;
+            out_valid_r <= 0;
         end else begin
-            if (in_handshake_w) begin
-                fifo_results_r[fifo_exctail_r] <= alu_result_w;
-                fifo_tags_r[fifo_exctail_r] <= op_tag_i;
-                fifo_exctail_r <= fifo_exctail_p1_w;
-            end
+            out_valid_r <= out_valid_next_w;
         end
     end
 
-    // Output handshake
-    logic out_handshake_w;
-    assign out_valid_o = ~fifo_empty_r;
-    assign out_handshake_w = out_valid_o & out_ready_i;
+    assign out_valid_o = out_valid_r;
+    assign in_ready_o = in_ready_w;
+
+    logic [N_THREADS-1:0][RLEN-1:0] result_w, result_r;
+    op_tag_s op_tag_r;
 
     always_ff @(posedge clk) begin
-        if (!rst_n) begin
-            fifo_head_r <= 0;
-        end else begin
-            if (out_handshake_w) begin
-                fifo_head_r <= fifo_head_p1_w;
-            end
+        if (in_fire_w) begin
+            result_r <= result_w;
+            op_tag_r <= op_tag_i;
         end
     end
-
-    // FIFO empty/full flag
-
-    always_ff @(posedge clk) begin
-        if (!rst_n) begin
-            fifo_empty_r <= 1;
-            fifo_full_r <= 0;
-        end else begin
-            if (out_handshake_w & in_handshake_w) begin
-                // This can only happen when the FIFO is neither full nor empty
-                // So keep both flags the same
-                fifo_empty_r <= 0;
-                fifo_full_r <= 0;
-            end else if (out_handshake_w) begin
-                fifo_empty_r <= fifo_almost_empty_w;
-                fifo_full_r <= 0;
-            end else if (in_handshake_w) begin
-                fifo_empty_r <= 0;
-                fifo_full_r <= fifo_almost_full_w;
-            end
-        end
-    end
+    assign result_o = result_r;
+    assign op_tag_o = op_tag_r;
 
     generate
         for (genvar I = 0; I < N_THREADS; I++) begin : gen_alu
@@ -114,7 +70,7 @@ module multialu
                 .instr_i(instr_i),
                 .warp_id_i(warp_id_i),
                 .pc_i(pc_i),
-                .result_o(alu_result_w[I]),
+                .result_o(result_w[I]),
                 .cycle_time_i(cycle_time_i),
                 .instret_i(instret_i[I]),
                 .wuinstret_i(wuinstret_i),
@@ -122,9 +78,6 @@ module multialu
             );
         end
     endgenerate
-
-    assign result_o = fifo_results_r[fifo_head_r];
-    assign op_tag_o = fifo_tags_r[fifo_head_r];
 
 endmodule
 
